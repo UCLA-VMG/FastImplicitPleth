53167b47ebca2ed86fa216fd9ebbf03c7a630d53
Anirudh0707 <yani.bh@gmail.com>
2023-01-09 01:40:58 -0800

    Add root and config as args to auto scripts

--- auto_dataset_motion.py
+++ auto_dataset_motion.py
@@ -3,13 +3,14 @@ import pickle
 import argparse
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('--root', required=True, type=str)
+parser.add_argument('--root', default='/home/pradyumnachari/Documents/ImplicitPPG/SIGGRAPH_Data/rgb_files', type=str)
 parser.add_argument('--pkl', required=True, type=str)
 parser.add_argument('--cuda-dev', required=True, type=str)
+parser.add_argument('--config-folder', default='configs/dataset', type=str)
 args = parser.parse_args()
 
 # ROOT_PATH = args.root
-ROOT_PATH = '/home/pradyumnachari/Documents/ImplicitPPG/SIGGRAPH_Data/rgb_files'
+ROOT_PATH = args.root
 
 # '/home/pradyumnachari/Documents/ImplicitPPG/SIGGRAPH_Data/fl_l1.pkl'
 with open(args.pkl,'rb') as f:
@@ -20,18 +21,30 @@ for folder in list_of_folders:
     filename = 'implicitpleth.scripts.train_double_hash_delta_motion'
     vp = os.path.join(ROOT_PATH, folder)
     # Start frame 0
-    config = 'configs/dataset/dhd_motion_0.json'
-    log = f'logs/{folder}_motion_log_0.txt'
-    os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder} --verbose > {log} ')
+    config = os.path.join(args.config_folder, f'dhd_motion_0.json')
+    if os.path.exists(config):
+        log = f'logs/{folder}_motion_log_0.txt'
+        os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder} --verbose > {log} ')
+    else:
+        print(f'{config} does not exist')
     
     
     # Start frame 300
-    config = 'configs/dataset/dhd_motion_300.json'
-    log = f'logs/{folder}_motion_log_300.txt'
-    os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder} --verbose > {log} ')
+    config = os.path.join(args.config_folder, f'dhd_motion_300.json')
+    if os.path.exists(config):
+        log = f'logs/{folder}_motion_log_300.txt'
+        os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder} --verbose > {log} ')
+    else:
+        print(f'{config} does not exist')
     
     
     # Start frame 600
-    config = 'configs/dataset/dhd_motion_600.json'
-    log = f'logs/{folder}_motion_log_600.txt'
-    os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder} --verbose > {log} ')
+    config = os.path.join(args.config_folder, f'dhd_motion_600.json')
+    if os.path.exists(config):
+        log = f'logs/{folder}_motion_log_600.txt'
+        os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder} --verbose > {log} ')
+    else:
+        print(f'{config} does not exist')
+
+    print(vp)
+    print("-"*50)
--- auto_dataset_residual.py
+++ auto_dataset_residual.py
@@ -3,13 +3,14 @@ import pickle
 import argparse
 
 parser = argparse.ArgumentParser()
-# parser.add_argument('--root', required=True, type=str)
+parser.add_argument('--root', default='/home/pradyumnachari/Documents/ImplicitPPG/SIGGRAPH_Data/rgb_files', type=str)
 parser.add_argument('--pkl', required=True, type=str)
 parser.add_argument('--cuda-dev', required=True, type=str)
+parser.add_argument('--config-folder', default='configs/dataset', type=str)
 args = parser.parse_args()
 
 # ROOT_PATH = args.root
-ROOT_PATH = '/home/pradyumnachari/Documents/ImplicitPPG/SIGGRAPH_Data/rgb_files'
+ROOT_PATH = args.root
 
 # '/home/pradyumnachari/Documents/ImplicitPPG/SIGGRAPH_Data/fl_l1.pkl'
 with open(args.pkl,'rb') as f:
@@ -20,21 +21,30 @@ for folder in list_of_folders:
     filename = 'implicitpleth.scripts.train_residual'
     vp = os.path.join(ROOT_PATH, folder)
     # Start frame 0
-    config = 'configs/dataset/residual_0.json'
-    log = f'logs/{folder}_residual_log_0.txt'
-    os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder}  --append_load_path _{folder}/epoch_10.pth --verbose > {log} ')
+    config = os.path.join(args.config_folder, f'residual_0.json')
+    if os.path.exists(config):
+        log = f'logs/{folder}_residual_log_0.txt'
+        os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder}  --append_load_path _{folder}/epoch_10.pth --verbose > {log} ')
+    else:
+        print(f'{config} does not exist')
     
     
     # Start frame 300
-    config = 'configs/dataset/residual_300.json'
-    log = f'logs/{folder}_residual_log_300.txt'
-    os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder}  --append_load_path _{folder}/epoch_10.pth --verbose > {log} ')
+    config = os.path.join(args.config_folder, f'residual_300.json')
+    if os.path.exists(config):
+        log = f'logs/{folder}_residual_log_300.txt'
+        os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder}  --append_load_path _{folder}/epoch_10.pth --verbose > {log} ')
+    else:
+        print(f'{config} does not exist')
     
     
     # Start frame 600
-    config = 'configs/dataset/residual_600.json'
-    log = f'logs/{folder}_residual_log_600.txt'
-    os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder}  --append_load_path _{folder}/epoch_10.pth --verbose > {log} ')
+    config = os.path.join(args.config_folder, f'residual_600.json')
+    if os.path.exists(config):
+        log = f'logs/{folder}_residual_log_600.txt'
+        os.system(f'CUDA_VISIBLE_DEVICES={args.cuda_dev} nohup python -m {filename} -vp {vp} -config {config} --append_save_path _{folder}  --append_load_path _{folder}/epoch_10.pth --verbose > {log} ')
+    else:
+        print(f'{config} does not exist')
     
-    print("-"*50)
     print(vp)
+    print("-"*50)
